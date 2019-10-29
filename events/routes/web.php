ddec625e4d743bc1d3004c1b885eb4a9562a44c3
Yasmin-Alhajjaj <yasmin.alhajjaj@gmail.com>
2019-10-29 23:14:04 +0200

    finish functins

--- events/routes/web.php
+++ events/routes/web.php
@@ -17,5 +17,8 @@ Route::resource ('/category','CategoryController');
 Route::resource ('/event','EventController');
 Route::resource ('/comment','CommentController');
 
+//Route::get('/home', 'EventController@info')->name('home');
+
+
 
 Route::get('/{event_id}/info', 'EventController@info')->name('info');
