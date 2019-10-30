b6b25b4a7c5f676bfad6a4a2cb30f20ab70f2a46
abd.maqableh <51744473+abd-maqableh@users.noreply.github.com>
2019-10-30 11:12:30 +0200

    resolve confilic

--- events/routes/web.php
+++ events/routes/web.php
@@ -25,6 +25,6 @@ Auth::routes();
 Route::get('/home', 'CategoryController@index')->name('home');
 Route::resource ('/category','CategoryController');
 Route::resource ('/event','EventController');
-// Maqableh Abooooooooooooood;
+//Maqableh Abooooooooooooood;
 
 
